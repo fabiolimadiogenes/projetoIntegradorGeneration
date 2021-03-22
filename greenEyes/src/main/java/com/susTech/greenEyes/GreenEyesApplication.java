9dcf1ed454e35da3661dda639fcc19eba15919ef
unknown <fabiolimadiogenes@gmail.com>
2021-03-22 14:09:12 -0300

    Enviando tarefa 3 e 4 criando a model Projeto Integrador

---
+++ greenEyes/src/main/java/com/susTech/greenEyes/GreenEyesApplication.java
@@ -0,0 +1,13 @@
+package com.susTech.greenEyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenEyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenEyesApplication.class, args);
+	}
+
+}
---
+++ greenEyes/src/test/java/com/susTech/greenEyes/GreenEyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susTech.greenEyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenEyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
