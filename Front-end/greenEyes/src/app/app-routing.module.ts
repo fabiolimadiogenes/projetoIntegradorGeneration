b3896197fc2f5d79dc92c547f1d6e56db659d6fc
unknown <fabiolimadiogenes@gmail.com>
2021-04-29 18:16:15 -0300

    Atualizando e corrigindo bugs

--- Front-end/greenEyes/src/app/app-routing.module.ts
+++ Front-end/greenEyes/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+
+  {path:"contato", component: ContatoComponent},
+  {path:"home", component: HomeComponent},
+  {path:"menu", component: MenuComponent},
+  {path:"rodape", component: RodapeComponent},
+  {path:"sobre-nos", component: SobreNosComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
