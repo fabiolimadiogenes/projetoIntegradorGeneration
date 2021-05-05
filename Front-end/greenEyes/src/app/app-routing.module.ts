7060d18fdf4cac8688896a3c8336cdc5f5c88e35
Konaaz <vinicius.melo.rodrigues@hotmail.com>
2021-05-05 16:28:20 -0300

    teste

--- Front-end/greenEyes/src/app/app-routing.module.ts
+++ Front-end/greenEyes/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
-import { CadastroComponent } from './cadastro/cadastro.component';
-import { LoginComponent } from './login/login.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
@@ -18,13 +17,8 @@ const routes: Routes = [
   {path:"menu", component: MenuComponent},
   {path:"rodape", component: RodapeComponent},
   {path:"sobre-nos", component: SobreNosComponent},
-<<<<<<< HEAD
-  {path:"cadastro", component:CadastroComponent},
-  {path:"login", component:LoginComponent}
-=======
   {path:"login", component: LoginComponent},
   {path:"cadastro", component: CadastroComponent},
->>>>>>> cb8de90f0013a887404d4bfea8e4883c003755fc
 
 ];
 
--- Front-end/greenEyes/src/app/cadastro/cadastro.component.ts
+++ Front-end/greenEyes/src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from '../model/Usuario';
-<<<<<<< HEAD
-import { Login } from '../model/Login';
-=======
->>>>>>> cb8de90f0013a887404d4bfea8e4883c003755fc
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -14,45 +10,6 @@ import { AuthService } from '../service/auth.service';
 })
 export class CadastroComponent implements OnInit {
 
-<<<<<<< HEAD
-  user: Usuario = new Usuario
-  confirmarSenha: string
-  tipoUsuario: string
-
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-    window.scroll(0, 0)
-
-  }
-
-  confirmSenha(event: any) {
-    this.confirmarSenha = event.target.value
-
-  }
-
-  tipoUser(event: any) {
-    this.tipoUsuario = event.target.value
-  }
-
-  cadastrar() {
-
-    if (this.user.senha != this.confirmarSenha) {
-      alert('A senha está incorreta')
-
-    } else {
-      this.authService.cadastrar(this.user).subscribe((resp: Usuario) => {
-        this.user = resp
-        this.router.navigate(['/entrar'])
-        alert('Usuario cadastrado com sucesso')
-      })
-
-    }
-
-=======
   user: Usuario = new Usuario()
   confirmarSenha: string
   tipoUsuario: string
@@ -69,7 +26,6 @@ export class CadastroComponent implements OnInit {
 
   confirmSenha(event: any){
     this.confirmarSenha = event.target.value
->>>>>>> cb8de90f0013a887404d4bfea8e4883c003755fc
   }
 
   tipoUser(event: any){
